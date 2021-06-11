491c3fff91d9ad22bea4d46542392bf6be21d09a
kirimaru90 <43108039+kirimaru90@users.noreply.github.com>
2021-06-12 01:15:16 +0200

    DeathObstacle and Respawn

--- Assets/Respawn.cs
+++ Assets/Respawn.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Respawn : MonoBehaviour
 {
+    [SerializeField] private GameObject RespawnPoint;
+    [SerializeField] private GameObject Character; 
+    
     void OnCollisionEnter2D(Collision2D col){
+        Debug.Log("collision");
         if(col.transform.CompareTag("Player")){
-            col.gameObject.GetComponent<CharacterMovement>().Respawn();
+            Character.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            Character.transform.position = RespawnPoint.transform.position;
         }
     }
 }
