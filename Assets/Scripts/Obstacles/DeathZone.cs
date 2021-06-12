84d0bf03d95263d70df3baa62d26d33101c6ccb2
kirimaru90 <43108039+kirimaru90@users.noreply.github.com>
2021-06-12 13:27:37 +0200

    Fall to Death Implemented

---
+++ Assets/Scripts/Obstacles/DeathZone.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : Respawn
+{
+    public override void OnCollisionEnter2D(Collision2D collision)
+    {
+        base.OnCollisionEnter2D(collision);
+    }
+}
