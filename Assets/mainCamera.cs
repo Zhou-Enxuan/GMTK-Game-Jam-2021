8344c8129224c6b5daa9e375ec91b838868c6b8b
kirimaru90 <43108039+kirimaru90@users.noreply.github.com>
2021-06-12 18:36:09 +0200

    Following Camera

---
+++ Assets/mainCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainCamera : MonoBehaviour
+{
+    private Vector3 targetPos;
+    [Range(5, 20)] [SerializeField] private float cameraDistance = 10;
+    [Range(-3, 3)] [SerializeField] private float cameraHeigth = 3;
+    [Range(-5, 5)] [SerializeField] private float cameraWidth = 0;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float margin = 1;
+
+    [SerializeField] private GameObject player;
+    [Range(1, 10)] [SerializeField] private float interpolationRatio = 2;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        targetPos = player.transform.position - offset;
+        transform.position = targetPos;
+    }
+
+    void FixedUpdate(){
+        follow();        
+    }
+
+    private void follow(){
+        offset = new Vector3(-cameraWidth, -cameraHeigth, cameraDistance);
+        targetPos = player.transform.position - offset;
+
+        if((transform.position - targetPos).magnitude >= margin){
+            transform.position = Vector3.Lerp(transform.position, targetPos, interpolationRatio*Time.fixedDeltaTime);
+        }
+    }
+}
