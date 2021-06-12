3b171e0373f81763e3038846c568275ad9a402c6
kirimaru90 <43108039+kirimaru90@users.noreply.github.com>
2021-06-12 02:03:20 +0200

    Respawn

--- Assets/Respawn.cs
+++ Assets/Respawn.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class Respawn : MonoBehaviour
 {
-    [SerializeField] private GameObject RespawnPoint;
-    [SerializeField] private GameObject Character; 
-    
     void OnCollisionEnter2D(Collision2D col){
-        Debug.Log("collision");
         if(col.transform.CompareTag("Player")){
-            Character.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            Character.transform.position = RespawnPoint.transform.position;
+            col.gameObject.GetComponent<CharacterMovement>().Respawn();
         }
     }
 }
