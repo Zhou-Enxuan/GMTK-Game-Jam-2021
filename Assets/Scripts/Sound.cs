9306d6225a876a22ec91fbba72bb3aa6f0193d98
Jose A. Rivera <jarrpa@gmail.com>
2021-06-13 00:29:30 -0500

    implement AudioManager in Initial scene
    Signed-off-by: Jose A. Rivera <jarrpa@gmail.com>

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+[ExecuteAlways]
+public class Sound {
+    public string name;
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume = 1f;
+    [Range(.1f, 3f)]
+    public float pitch = 1f;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+}
